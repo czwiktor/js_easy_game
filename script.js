f567eaa3b965c8008d0163dea714b1ff969777ec
Wiktor Czech <czwiktor@gmail.com>
2020-07-23 13:06:43 +0200

    Bug fixed (infinite loop) and added passing of time

--- script.js
+++ script.js
@@ -3,17 +3,48 @@ let time = 60; // in seconds
 let mole = null;
 let timeCont = null;
 let scoreCont = null;
-
+let timeIntId = null;
 const mole_wh = 60;
 const mole_hg = 85;
 
 
 let mole_action_time = 2; // in seconds
 
+const createTimeContainer = () => {
+    const div = document.createElement("div");
+    div.style.position = "fixed";
+    div.style.right = 0 + "px";
+    div.style.top = 0 + "px" ; 
+    document.body.appendChild(div);
+    timeCont = div
+    displayTime();
+}
+
+const createScrContainer = () => {
+    const div = document.createElement("div");
+    div.style.position = "fixed";
+    div.style.left = 0 + "px";
+    div.style.top = 0 + "px" ; 
+    document.body.appendChild(div);
+    scoreCont = div
+    console.log("elo")
+    displayScore();
+}
+
+
+const displayScore = () => {
+    scoreCont.innerText = score + " points";
+}
+
+const displayTime = () => {
+    timeCont.innerHTML = time + " seconds left";
+}
+
 
 const timeTicker =  () => {
     time = time - 1;
     displayTime();
+    if (time === 0) endGame()
 }
 
 
@@ -23,7 +54,7 @@ const addScore = () => {
 }
 
 const posRandomizer = (min,max) => {
-    return Math.floor(Math.random()*(max-min+1)+50);
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 const randomizeMolePos = () => {
@@ -35,7 +66,7 @@ const randomizeMolePos = () => {
 
 const moleRemover = () => {
     if(mole===null) return
-    else
+  
     mole.remove();
 }
 
@@ -48,13 +79,13 @@ const moleMaker = () => {
     div.style.position = "fixed";
     div.style.left = x + "px";
     div.style.top = y + "px" ; 
-    div.addEventListener("click",moleClicker());
+    div.addEventListener('click',moleClicker);
     document.body.appendChild(div);
     mole = div;
 }
 
 const createMole = () => {
-        andomizeMolePos();
+        randomizeMolePos();
         moleMaker();
 }
 const moleClicker = () => {
@@ -62,38 +93,29 @@ const moleClicker = () => {
         createMole();
 }
 
-
-const displayScore = () => {
-    scoreCont.innerText = score + " points";
+const resetGame =  () => {
+    window.location = '';
 }
 
-const createScrContainer = () => {
-     const div = document.createElement("div");
-     div.style.position = "fixed";
-     div.style.left = 0 + "px";
-     div.style.top = 0 + "px" ; 
-     document.body.appendChild(div);
-     scoreCont = div
-     displayScore();
-     console.log("elo")
+
+const startInterval = () => {
+    timeIntId = setInterval(
+        timeTicker,
+        1000
+    )
 }
 
-const displayTime = () => {
-    timeCont.innerHTML = time + " seconds left";
+const endGame = () => {
+    resetGame();
+    alert("Game ended! Your score: " + score)
+    
 }
 
-const createTimeContainer = () => {
-     const div = document.createElement("div");
-     div.style.position = "fixed";
-     div.style.right = 0 + "px";
-     div.style.top = 0 + "px" ; 
-     document.body.appendChild(div);
-     timeCont = div
-     displayTime();
-}
+const init = () => {
+     createScrContainer();
+     createTimeContainer();
+     createMole();
+     startInterval();
+};
 
-    createScrContainer();
-    createTimeContainer();
-    randomizeMolePos();
-    moleMaker();
-
+init();
