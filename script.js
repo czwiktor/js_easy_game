3c2c3d9cd4619cbce8fb3233adb53402e10f5ec0
Wiktor Czech <czwiktor@gmail.com>
2020-07-24 17:02:49 +0200

    Added graphics, mole appearance speed up, time passing and score counting with max possible score.

--- script.js
+++ script.js
@@ -1,55 +1,86 @@
 let x, y, score = 0;
+let maxScore = -1;
 let time = 60; // in seconds
 let mole = null;
 let timeCont = null;
 let scoreCont = null;
 let timeIntId = null;
-const mole_wh = 60;
-const mole_hg = 85;
-
-
-let mole_action_time = 2; // in seconds
+const playfield = document.getElementById("play_field");
+const playfield_p = window.getComputedStyle(document.getElementById("play_field"), null)
+const mole_wh = 120;
+const mole_hg = 93;
+const header_p = window.getComputedStyle(document.getElementById("header"), null)
+const header = document.getElementById("header");
+let mole_action_time_id=null;
+let mole_action_time = 3; // in seconds
+let mole_action_time_inc = 10; // in seconds
 
 const createTimeContainer = () => {
     const div = document.createElement("div");
-    div.style.position = "fixed";
-    div.style.right = 0 + "px";
-    div.style.top = 0 + "px" ; 
-    document.body.appendChild(div);
+    div.style.margin= "auto";
+    div.style.textAlign = "center";
+    div.style.padding = "15px";
+    div.style.display ="inline-block" ;
+    div.style.width ="100%"; 
+    header.appendChild(div);
     timeCont = div
     displayTime();
 }
 
 const createScrContainer = () => {
     const div = document.createElement("div");
-    div.style.position = "fixed";
-    div.style.left = 0 + "px";
-    div.style.top = 0 + "px" ; 
-    document.body.appendChild(div);
+    div.style.margin= "auto";
+    div.style.padding = "15px";
+    div.style.textAlign = "center";
+    div.style.display ="inline-block" ; 
+    div.style.width ="100%";
+    header.appendChild(div);
     scoreCont = div
     console.log("elo")
+    maxScore+=1;
     displayScore();
 }
 
+const stopMoleRotation = () => {
+    if (mole_action_time === null) return;
+    clearInterval(mole_action_time_id);
+}
 
+const startMoleRotation = () => {
+    stopMoleRotation()
+    mole_action_time_id = setInterval(createMole, mole_action_time*1000)
+
+}
 const displayScore = () => {
-    scoreCont.innerText = score + " points";
+    scoreCont.innerText = score + " points ";
+    scoreCont.innerText += " out of " + maxScore;
 }
 
 const displayTime = () => {
     timeCont.innerHTML = time + " seconds left";
 }
 
+const pushTheTempo = () => {
+    if (time % mole_action_time_inc !== 0) return
+    if (mole_action_time > 1){
+        mole_action_time /=2;
+    }
+    else
+        mole_action_time = 0.7;
+        return;
 
+}
 const timeTicker =  () => {
     time = time - 1;
     displayTime();
+    pushTheTempo();
     if (time === 0) endGame()
 }
 
 
 const addScore = () => {
     score = score + 10;
+    mole.style.backgroundImage = "url('krecik_po.png')";
     displayScore();
 }
 
@@ -58,10 +89,11 @@ const posRandomizer = (min,max) => {
 }
 
 const randomizeMolePos = () => {
-    const visual_hg = window.innerHeight-mole_hg;
-    const visual_wh = window.innerWidth-mole_wh;
-    x = posRandomizer(0,visual_wh);
-    y = posRandomizer(0,visual_hg);
+    const top_margin = parseInt(header_p.getPropertyValue("height") + 5);
+    const visual_hg = parseInt(playfield_p.getPropertyValue("height"))-mole_hg;
+    const visual_wh = parseInt(playfield_p.getPropertyValue("width"))-mole_wh;
+    x = posRandomizer(30,visual_wh);
+    y = posRandomizer(top_margin,visual_hg);
 }
 
 const moleRemover = () => {
@@ -75,22 +107,31 @@ const moleMaker = () => {
     const div = document.createElement("div");
     div.style.width = mole_wh + "px";
     div.style.height = mole_hg + "px";
-    div.style.backgroundColor = "black";
+    div.style.backgroundImage = "url('krecik.png')";
+    div.style.backgroundClip = "content-box";
     div.style.position = "fixed";
     div.style.left = x + "px";
-    div.style.top = y + "px" ; 
+    div.style.top =  y + "px" ; 
     div.addEventListener('click',moleClicker);
-    document.body.appendChild(div);
+    playfield.appendChild(div);
     mole = div;
+    addMaxScore();
+}
+const addMaxScore = () => {
+    maxScore +=10;
+    displayScore();
 }
 
 const createMole = () => {
         randomizeMolePos();
         moleMaker();
+        startMoleRotation();
+       
 }
 const moleClicker = () => {
         addScore();
-        createMole();
+        setTimeout(createMole,500)
+       
 }
 
 const resetGame =  () => {
